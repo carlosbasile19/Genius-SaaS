50ab239f45e344c4a26b10ffc74b4e828bbc1d74
Carlitos <basilecarlos12@gmail.com>
2024-01-12 08:40:41 +0100

    Sidebar Completed

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,13 +1,10 @@
-import { UserButton } from "@clerk/nextjs"
 
 
 export default function DashboardPage() {
   return (
     <div>
       <p>Dashboard Page! (protected) </p>  
-      <UserButton afterSignOutUrl="/">
-
-      </UserButton>
+      
     </div>
      
   )
