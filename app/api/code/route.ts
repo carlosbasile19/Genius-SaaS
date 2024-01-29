cddd51ea252dced50aad61c445643d173ae6796e
Carlitos <basilecarlos12@gmail.com>
2024-01-29 19:17:32 +0100

    Code Generation Completed

---
+++ app/api/code/route.ts
@@ -0,0 +1,43 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import OpenAI from "openai";
+const openai = new OpenAI();
+
+interface ChatCompletionRequestMessage {
+    role: 'user' | 'assistant' | 'system';
+    content: string;
+    name?: string;
+  }
+
+const instructionMessage: ChatCompletionRequestMessage = {
+    role: 'system',
+    content: 'You are a code generator. You must answer only in markdown code snippets. Use code comments for explanations.',
+}
+
+
+export async function POST(
+    req: Request
+) {
+    try{
+        const { userId} = auth();
+        const body = await req.json();
+        const {messages} = body;
+
+        if(!userId)
+            return new NextResponse("Unauthorized", {status: 401})
+        
+        if(!messages)
+            return new NextResponse("Message is required", {status: 400})
+
+        const response = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [instructionMessage, ...messages],
+          });
+
+        return NextResponse.json(response.choices[0].message)
+
+    }catch (e) {
+        console.error("[CODE_ERROR]",e);
+        return new NextResponse("Internal error", {status: 500})
+    }
+}
